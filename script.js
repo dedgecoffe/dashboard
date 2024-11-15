d701b3eecc64441f265a1b867fc6f0c6a232c582
MuhammadMallq <Kamalputra1177@gmail.com>
2024-11-15 14:32:12 +0700

    tes

--- script.js
+++ script.js
@@ -41,5 +41,5 @@ setInterval(() => {
 
 // Logout functionality
 document.getElementById('logoutBtn').addEventListener('click', function(event) {
-    window.location.href = 'https://dedgecoffe.github.io/login/';
+    window.location.href = 'https://dedgecoffee.id.biz.id/login/';
 });
